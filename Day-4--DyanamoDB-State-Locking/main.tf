a4864c8d3eec3d92137b8cc8259639980cc845d3
SoumyaDora <soumyadora143@gmail.com>
2025-04-21 17:41:25 +0530

    LifecyleRules Uodated

--- Day-4--DyanamoDB-State-Locking/main.tf
+++ Day-4--DyanamoDB-State-Locking/main.tf
@@ -4,7 +4,16 @@ resource "aws_instance" "name" {
     availability_zone = "ap-south-1a"
 
     tags = {
-      Name = "Sam"
+      Name = "Sam---Sung>"
     }
+  #   lifecycle {
+    # prevent_destroy = true
+    #}
+    #   lifecycle {
+#     ignore_changes = [ tags,]
+#   }
+#  lifecycle {
+#    create_before_destroy = true
+#  }
   
 }
